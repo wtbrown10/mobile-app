fe0987215077a46c33af7383e30721b53400ae23
will_brown10 <wlemogar.brown@nordsonefd.com>
2022-05-17 14:04:56 -0400

    entity class

--- src/main/java/com/will/mobile_app/io/entity/UserEntity.java
+++ src/main/java/com/will/mobile_app/io/entity/UserEntity.java
@@ -1,4 +1,90 @@
 package com.will.mobile_app.io.entity;
 
-public class UserEntity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity(name="users")
+public class UserEntity implements Serializable {
+
+    private static final long serialVersionUID =
+            6081189768764447543L;
+
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column(nullable = false)
+    private String userId;
+    @Column(nullable = false, length = 50)
+    private String firstName;
+    @Column(nullable = false, length = 50)
+    private String lastName;
+    @Column(nullable = false, length = 120)
+    private String email;
+    @Column(nullable = false)
+    private String encryptedPassword;
+    @Column(nullable = false)
+    private String emailVerificationToken;
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private Boolean emailVerificationStatus;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public String getEmailVerificationToken() {
+        return emailVerificationToken;
+    }
+
+    public void setEmailVerificationToken(String emailVerificationToken) {
+        this.emailVerificationToken = emailVerificationToken;
+    }
+
+    public Boolean getEmailVerificationStatus() {
+        return emailVerificationStatus;
+    }
+
+    public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
+        this.emailVerificationStatus = emailVerificationStatus;
+    }
+
+
 }
