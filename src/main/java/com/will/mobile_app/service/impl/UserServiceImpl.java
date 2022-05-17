4f7127e7ec293c53733f085ac76847fee86e3e4f
will_brown10 <wlemogar.brown@nordsonefd.com>
2022-05-17 14:42:32 -0400

    autowired userRepo into service class

--- src/main/java/com/will/mobile_app/service/impl/UserServiceImpl.java
+++ src/main/java/com/will/mobile_app/service/impl/UserServiceImpl.java
@@ -1,13 +1,35 @@
 package com.will.mobile_app.service.impl;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.will.mobile_app.UserRepository;
+import com.will.mobile_app.io.entity.UserEntity;
 import com.will.mobile_app.service.UserService;
 import com.will.mobile_app.shared.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static com.fasterxml.jackson.databind.util.BeanUtil.*;
+import static org.springframework.beans.BeanUtils.copyProperties;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    UserRepository userRepository;
     @Override
     public UserDTO createUser(UserDTO user) {
-        return null;
+
+        UserEntity userEntity = new UserEntity();
+        copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("test");
+
+        userEntity.setUserId("testUserId");
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+
+        UserDTO returnValue = new UserDTO();
+        copyProperties(storedUserDetails, returnValue);
+
+        return returnValue;
     }
 }
